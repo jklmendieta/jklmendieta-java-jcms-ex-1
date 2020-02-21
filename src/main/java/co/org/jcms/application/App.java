a7cd34f806b440e24c1075e347d6f5ea4da5db61
Juan Camilo Mendieta Silva <1032370759@ccb.org.co>
2020-02-21 16:44:40 -0500

    inicial 2

--- src/main/java/co/org/jcms/application/App.java
+++ src/main/java/co/org/jcms/application/App.java
@@ -4,6 +4,7 @@ public class App {
 
 	public static void main(String[] args) {
 		System.out.println("Example Message for java-jcms-ex-1");
+		System.out.println("Example Message for java-jcms-ex-1");
 
 	}
 
