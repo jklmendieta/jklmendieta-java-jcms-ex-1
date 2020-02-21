221e2282f5094e7e22401af7bb1170b93bc851cc
Juan Camilo Mendieta Silva <1032370759@ccb.org.co>
2020-02-21 17:23:36 -0500

    Inicio de configuración basica - spring boot - habilita ínicio por consola

--- src/main/java/co/org/jcms/application/App.java
+++ src/main/java/co/org/jcms/application/App.java
@@ -1,10 +1,26 @@
 package co.org.jcms.application;
 
-public class App {
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// 1 - SpringBoot Configuration - Inicializa Spring Boot Application e implementa command line runner para habilitar la consola de comandos
+@SpringBootApplication
+public class App implements CommandLineRunner {
 
 	public static void main(String[] args) {
-		System.out.println("Example Message for java-jcms-ex-1");
-		System.out.println("Example Message for java-jcms-ex-1");
+
+		
+		// 1 - SpringBoot Configuration - Inicializa el microservicio con SpringBoot - Base de Inicio
+		SpringApplication.run(App.class, args);
+
+	}
+
+	// 1 - SpringBoot Configuration - Implementa run
+	@Override
+	public void run(String... args) throws Exception {
+
+		System.out.println("El micro servicio Spring Boot para  java-jcms-ex-1 ha iniciado");
 
 	}
 
